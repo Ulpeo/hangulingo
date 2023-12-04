555e00a966d8306f8fc405f853c693646b8d9294
Emma Meline <emma.meline@edu.ece.fr>
2023-12-04 14:54:20 +0900

    score managing

--- game.py
+++ game.py
@@ -1,14 +1,53 @@
 import random
 
+def delete_line(file_name, line_number):
+    # Read the file and store each line in a list
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+
+    # Check if the line number is valid
+    if 1 <= line_number <= len(lines):
+        # Delete the specified line
+        del lines[line_number - 1]
+
+        # Rewrite the file with the remaining lines
+        with open(file_name, 'w') as modified_file:
+            modified_file.writelines(lines)
+        print(f"Line {line_number} successfully deleted.")
+    else:
+        print("Invalid line number.")
+
+
+def save(pseudo, points):
+    save_file = open("scores.txt",'r+')
+    lines = save_file.readlines()
+    count =0
+
+    for line in lines:
+        count += 1
+        line = line.rstrip()
+        part = line.split(" ")
+        if (part[0] == pseudo):
+            found = True
+            delete_line("scores.txt", count)
+            save_file.close()
+        else:
+            save_file.close()
+    save_file = open("scores.txt",'a+')
+
+    save_file.write(pseudo +" "+str(points)+"\n")
+    save_file.close()
+
+
 def load_words_from_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     return [line.strip().split('; ') for line in lines]
 
-def main():
+def game(pseudo, total_points):
     file_path = "korean_verbs.txt"
     word_list = load_words_from_file(file_path)
-    total_points = 0
+    total_points = int(total_points)
 
     print("Welcome to the word game!")
     print("Type 'quit' to exit the game.")
@@ -31,6 +70,7 @@ def main():
             if player_answer.lower() == 'quit':
                 print("Thanks for playing! Goodbye.")
                 print(f"Your total points: {total_points}")
+                save(pseudo, total_points)
                 return
             elif player_answer.lower() == correct_answer.lower():
                 print("Correct! Well done.\n")
@@ -38,5 +78,5 @@ def main():
             else:
                 print(f"Wrong. The correct answer is '{correct_answer}'.\n")
 
-if __name__ == "__main__":
-    main()
+
+
--- menu.py
+++ menu.py
@@ -17,7 +17,21 @@ option = int(input("\nEnter 1 to start a game\n 2 to resume a game\n 3 to add a 
 if option == 1:
     pseudo = input("enter your pseudo")
     score = 0
-    game(pseudo, 0)
+
+    score_file = open("scores.txt", "r")
+
+    # search score in the file
+    lines = score_file.readlines()
+
+    for line in lines:
+        line = line.rstrip()
+        str = line.split(" ")
+        if (str[0] == pseudo):
+            found = True
+            print("the pseudonyme is already used")
+            break
+    else:
+        game(pseudo, 0)
 
 elif option == 2:
     pseudo = input("enter your pseudo")
